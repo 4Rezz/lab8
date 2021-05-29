47b1dc75093faace5be85494745d1e2c94be5200
Roman Ilyshun <romanilyshun@gmail.com>
2021-05-29 13:56:38 +0300

    lab8_2

---
+++ Test/Test.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8_1_2/lab_8_1_2.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test
+{
+	TEST_CLASS(Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char s[10] = "groooot";
+			char a[10] = "gr**t";
+			Change(s);
+			Assert::AreEqual(s, a);
+		}
+	};
+}
